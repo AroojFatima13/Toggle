881409b6a2390244a62fb1a4fed4252fc29b834d
AroojFatima13 <arooj11112@gmail.com>
2021-12-12 00:15:58 -0800

    Toggle class

---
+++ eventlistener.js
@@ -0,0 +1,31 @@
+myFunction = () => {
+  
+  return document.getElementById("demo").innerHTML += "Name is Arooj<br>";
+
+}
+var x = document.getElementById("myBtn");
+
+  
+
+x.addEventListener("mouseover", myFunction);
+// x.addEventListener("click", mySecondFunction);
+// x.addEventListener("mouseout", myThirdFunction);
+
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML += "Arooj<br>"
+// }
+
+// function mySecondFunction() {
+    
+
+    
+//     document.getElementById("demo").innerHTML += ""
+    
+  
+// }
+
+// function myThirdFunction() {
+//   document.getElementById("demo").innerHTML += "<h2>Third Function<br></h2>"
+// }
+
